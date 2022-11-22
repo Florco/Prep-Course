075448446b0fd46f5c1899251fb12f27f1b05ffa
Florco <FlorenciaColautti99@hotmail.com>
2022-11-22 16:01:41 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -83,7 +83,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90) {
+  if (num > 90) {
     return true;
   } else {
     return false;
